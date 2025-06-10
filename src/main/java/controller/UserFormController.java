38f688202fd293fbdb1fe0a021fc20294244b68f
weerawardanakrishantha <weerawardanakrishantha@gmail.com>
2025-06-10 10:20:26 +0530

    login page

--- src/main/java/controller/UserFormController.java
+++ src/main/java/controller/UserFormController.java
@@ -1,14 +1,20 @@
 package controller;
 
+import db.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserFormController {
 
@@ -20,7 +26,7 @@ public class UserFormController {
 
     @FXML
     void btnLoginOnAction(ActionEvent event) {
-
+        login();
     }
 
     @FXML
@@ -30,4 +36,25 @@ public class UserFormController {
         stage.show();;
     }
 
+    public void login(){
+        try {
+            Connection connection= DBConnection.getInstance().getConnection();
+            PreparedStatement pst = connection.prepareStatement("select * from users where name=? and password=?");
+            pst.setString(1,txtUsername.getText());
+            pst.setString(2,txtPassword.getText());
+            ResultSet resultSet = pst.executeQuery();
+            if(resultSet.next()){
+                new Alert(Alert.AlertType.INFORMATION,"Login success").show();
+                Stage stage=new Stage();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/home.fxml"))));
+                stage.show();
+            }else {
+                new Alert(Alert.AlertType.INFORMATION,"Invalid Username or password").show();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
